41fd9ca4e174412e671b38b98dd884b92327e530
weewonwe <Humwestfree@gmail.com>
2025-02-22 14:35:06 +0900

    遷移アニメーション適応+テキストアウトライン
    ボタンクリック時の画面遷移にアニメーションを適応+見やすさ向上のためメインフィールドのテキストにアウトラインを適応
    一応もし実行できなかった場合はnpm install framer-motionでインストールすれば動くはず

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css'
-import ASTTree from './pages/ASTPage.tsx'
-import LoginPage from './pages/LoginPage'
 import Header from './components/Header.tsx'
-import SearchPage from './pages/SearchPage.tsx'
-import UploadPage from './pages/UploadPage.tsx'
 import { AuthProvider } from './context/AuthProvider'
+import AnimApp from './AnimationApp.tsx'
+
 function App() {
     return (
         <>
             <AuthProvider>
-                <BrowserRouter>
+                <Router basename="/">
                     <Header />
-                    <Routes>
-                        <Route path="/" element={<ASTTree />} />
-                        <Route path="/search" element={<SearchPage />} />
-                        <Route path="/upload" element={<UploadPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                    </Routes>
-                </BrowserRouter>
+                    <AnimApp />
+                </Router>
             </AuthProvider>
         </>
 
