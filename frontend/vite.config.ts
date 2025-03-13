81cd6f0be7994b634a9f01d90764570c0291c927
tamura <tomoki0927.7939@icloud.com>
2025-03-14 04:59:33 +0900

    fix: update Vite server config to allow ALB host access #38

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true
-  }
+    plugins: [react(), tailwindcss()],
+    server: {
+        host: true,
+        allowedHosts: [
+            'ast-generator-alb-1761937711.ap-northeast-1.elb.amazonaws.com'
+        ]
+    }
 })
