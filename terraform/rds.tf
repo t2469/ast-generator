b1bf70454ff0d0754e0820ed0a9f4c8975db5416
tamura <tomoki0927.7939@icloud.com>
2025-04-13 02:21:14 +0900

    Update Terraform configs for AWS (#44)

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,14 +1,18 @@
-resource "aws_db_subnet_group" "main" {
-  name = "db-subnet-group"
-  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_c.id]
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name = "${var.project_name}-rds-subnet"
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_c.id
+  ]
 
   tags = {
-    Name = "${var.project_name}-db-subnet-group"
+    Name    = "${var.project_name}-rds-subnet"
+    Project = var.project_name
   }
 }
 
 resource "aws_security_group" "rds_sg" {
-  name        = "rds-sg"
+  name        = "${var.project_name}-rds-sg"
   description = "Security group for RDS instance"
   vpc_id      = aws_vpc.main.id
 
@@ -16,7 +20,7 @@ resource "aws_security_group" "rds_sg" {
     from_port = 3306
     to_port   = 3306
     protocol  = "tcp"
-    security_groups = [aws_security_group.ecs_service_sg.id]
+    security_groups = [aws_security_group.api_service_sg.id]
   }
 
   egress {
@@ -25,20 +29,30 @@ resource "aws_security_group" "rds_sg" {
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name    = "${var.project_name}-rds-sg"
+    Project = var.project_name
+  }
 }
 
 resource "aws_db_instance" "db" {
-  identifier          = "db-instance"
-  engine              = "mysql"
-  engine_version      = "8.0"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 20
-  username            = var.db_username
-  password            = var.db_password
-  db_name             = var.db_name
-  publicly_accessible = false
+  identifier           = "${var.project_name}-mysql"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  username             = var.db_username
+  password             = var.db_password
+  db_name              = var.db_name
+  publicly_accessible  = false
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+
   skip_final_snapshot = true
 
-  db_subnet_group_name = aws_db_subnet_group.main.name
+  tags = {
+    Name    = "${var.project_name}-postgres"
+    Project = var.project_name
+  }
 }
